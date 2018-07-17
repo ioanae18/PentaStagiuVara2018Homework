231b3d8d8ddd9b08166b3933d1117ac50381daaf
Emilia Ioana <emmiii_xd@windowslive.com>
2018-07-17 17:42:14 +0300

    Week 01 + Week 02 Finished

--- Module03Week01/Module03Week01/Controllers/PostsController.cs
+++ Module03Week01/Module03Week01/Controllers/PostsController.cs
@@ -10,12 +10,13 @@ namespace Module03Week01.Controllers
     public class PostsController : Controller
     {
 
-		//public static List<Post> posts = new List<Post>();
-		//public static int Id = 0;
+		public static List<Post> posts = new List<Post>();
+		public static int Id = 0;
 
         // GET: Posts
         public ActionResult Index()
         {
+			/*
 			Post post = new Post
 			{
 				ID = 1,
@@ -26,8 +27,10 @@ namespace Module03Week01.Controllers
 				Priority = 0,
 				postType = PostType.Text
 			};
+			*/
 
-            return View(post);
+			ViewBag.DT = DateTime.Now;
+			return View(posts);
         }
 
 		public ActionResult Details()
@@ -48,6 +51,56 @@ namespace Module03Week01.Controllers
 				return HttpNotFound();
 			else
 			    return View("Details", post);
+		}
+
+		[HttpGet]
+		public ActionResult Delete(int id)
+		{
+			Post post = posts.Find(p => p.ID == id);
+
+			if (post == null)
+			{
+				return HttpNotFound();
+			}
+
+			posts.Remove(post);
+			return View(posts);
+		}
+
+		[HttpPost, ActionName("Delete")]
+		public ActionResult DeleteConfirmed(int id)
+		{
+			Post post = posts.Find(p => p.ID == id);
+			posts.Remove(post);
+			return RedirectToAction("Index");
+		}
+
+		[HttpGet]
+		public ActionResult Edit(int id)
+		{
+			Post post = posts.Find(u => u.ID == id);
+
+			if (post == null)
+			{
+				return HttpNotFound();
+			}
+
+			return View(post);
+		}
+
+		[HttpPost]
+		public ActionResult Edit(Post post)
+		{
+			Post findPost = posts.Find(p => p.ID == post.ID);
+			List<Post> result = new List<Post>();
+
+			foreach (Post p in posts)
+			{
+				if (!p.Equals(findPost)) result.Add(p); else result.Add(post);
+			}
+
+			posts = result;
+			return RedirectToAction("Index");
 		}
 	}
 }
